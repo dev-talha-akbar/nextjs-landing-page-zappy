68a1d99648ccac813ab10d86b072986dc2b97186
Talha Akbar <dev.talha.akbar@gmail.com>
2024-06-06 01:29:32 +0200

    set light theme as default

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,10 +38,10 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
