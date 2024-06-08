0dd25e55aa77f87da050b6c47f4939e6548b02cd
Talha Akbar <dev.talha.akbar@gmail.com>
2024-06-08 04:49:15 +0200

    implement navbar responsiveness

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,10 @@ import { PhoneIcon, Logo } from "@/components/icons";
 export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full gap-8" justify="start">
+      <NavbarContent
+        className="basis-1/5 sm:basis-full gap-2 md:gap-8"
+        justify="start"
+      >
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo className="text-primary" />
@@ -45,11 +48,8 @@ export const Navbar = () => {
         </ul>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
+      <NavbarContent className="flex basis-1/5 sm:basis-full" justify="end">
+        <NavbarItem className="flex gap-2">
           <Link
             isExternal
             aria-label="Twitter"
@@ -60,39 +60,19 @@ export const Navbar = () => {
             <span>1.800.847.298</span>
           </Link>
         </NavbarItem>
-        <NavbarItem className="hidden md:flex gap-2">
-          <Button variant="light">Login</Button>
+        <NavbarItem className="hidden lg:flex">
           <Button color="primary" variant="solid">
             Start free trial
           </Button>
         </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          Github
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
+        <NavbarMenuToggle className="lg:hidden" />
       </NavbarContent>
 
       <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
+        <div className="mx-2 mt-2 flex flex-col gap-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarMenuItem key={item.href}>
+              <Link href={item.href}>{item.label}</Link>
             </NavbarMenuItem>
           ))}
         </div>
